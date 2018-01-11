4ea47ed6767f13eee2019a900e52c9b953025b34
Yujia Lian <yujia.lian001@gmail.com>
2018-01-10 19:20:48 -0500

    usercenter/mobile headshot upload

--- react_news/src/js/components/mobile_usercenter.js
+++ react_news/src/js/components/mobile_usercenter.js
@@ -8,6 +8,7 @@ import {
   Tabs,
   message,
   Form,
+  Upload,
   Input,
   Button,
   CheckBox,
@@ -20,7 +21,34 @@ const TabPane = Tabs.TabPane;
 const MenuItemGroup = Menu.ItemGroup;
 import {Link, Router, Route, browserHistory} from 'react-router-dom';
 export default class MobileUserCenter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      previewImage:'',
+      previewVisible: false
+    }
+  };
   render() {
+    const props = {
+      action: 'http://newsapi.gugujiankong.com/handler.ashx',
+      headers: {
+        "Access-Control-Allow-Origin":'*'
+
+      },
+      listType: 'picture-card',
+      defaultFileList:[
+        {
+          uid:-1,
+          name: 'xxx.png',
+          state: 'done',
+          url: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png',
+          thmbUrl:'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png'
+        }
+      ],
+      onPreview: (file)=>{
+        this.setState({previewImage:file.url, previewVisible:true});
+      }
+    }
     return (<div>
       <MobileHeader/>
       <Row>
@@ -28,7 +56,18 @@ export default class MobileUserCenter extends React.Component {
           <Tabs>
             <TabPane tab="My collections" key="1"></TabPane>
             <TabPane tab="My Comments" key="2"></TabPane>
-            <TabPane tab="Headshot Setting" key="3"></TabPane>
+            <TabPane tab="Headshot Setting" key="3">
+              <div class="clearfix">
+                <Upload {...props}>
+                  <Icon type="plus"/>
+                  <div className="ant-upload-text">Upload</div>
+                </Upload>
+                <Modal visible={this.state.previewVisible} footer={null}
+                  onCancel={this.handleCancel}>
+                  <img alt="Preview" src={this.state.previewImage} />
+                </Modal>
+              </div>
+            </TabPane>
           </Tabs>
         </Col>
       </Row>
--- react_news/src/js/components/pc_usercenter.js
+++ react_news/src/js/components/pc_usercenter.js
@@ -9,6 +9,7 @@ import {
   message,
   Form,
   Input,
+  Upload,
   Button,
   CheckBox,
   Modal,
@@ -20,7 +21,35 @@ const TabPane = Tabs.TabPane;
 const MenuItemGroup = Menu.ItemGroup;
 import {Link, Router, Route, browserHistory} from 'react-router-dom';
 export default class PCUserCenter extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      previewImage:'',
+      previewVisible: false
+    }
+  };
   render() {
+    const props = {
+      action: 'http://newsapi.gugujiankong.com/handler.ashx',
+      headers: {
+        "Access-Control-Allow-Origin":'*'
+
+      },
+      listType: 'picture-card',
+      defaultFileList:[
+        {
+          uid:-1,
+          name: 'xxx.png',
+          state: 'done',
+          url: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png',
+          thmbUrl:'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png'
+        }
+      ],
+      onPreview: (file)=>{
+        this.setState({previewImage:file.url, previewVisible:true});
+      }
+    }
     return (<div>
       <PCHeader/>
       <Row>
@@ -30,7 +59,18 @@ export default class PCUserCenter extends React.Component {
           <Tabs>
             <TabPane tab="My collections" key="1"></TabPane>
             <TabPane tab="My Comments" key="2"></TabPane>
-            <TabPane tab="Headshot Setting" key="3"></TabPane>
+            <TabPane tab="Headshot Setting" key="3">
+              <div class="clearfix">
+                <Upload {...props}>
+                  <Icon type="plus"/>
+                  <div className="ant-upload-text">Upload</div>
+                </Upload>
+                <Modal visible={this.state.previewVisible} footer={null}
+                  onCancel={this.handleCancel}>
+                  <img alt="Preview" src={this.state.previewImage} />
+                </Modal>
+              </div>
+            </TabPane>
           </Tabs>
         </Col>
         <Col span={2}></Col>
